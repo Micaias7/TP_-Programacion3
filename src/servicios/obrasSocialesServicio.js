1e3997097ac15f3a0abda57145293fdad37d201d
EmilianaABaglioni <emi.baglioni@outlook.com>
2026-05-08 01:00:45 -0300

    actualizar en servicios

---
+++ src/servicios/obrasSocialesServicio.js
@@ -0,0 +1,21 @@
+import ObrasSociales from "../db/obrasSociales.js";
+
+export default class ObrasSocialesServicio {
+
+  constructor() {
+    this.obrasSociales = new ObrasSociales();
+  };
+
+  buscarTodas = () => {
+    return this.obrasSociales.buscarTodas();
+  };
+
+  buscarPorId = (id_obra_social) => {
+    return this.obrasSociales.buscarPorId(id_obra_social);
+  };
+
+  actualizar = (id_obra_social, nombre, descripcion, porcentaje_descuento, es_particular) => {
+    return this.obrasSociales.actualizar(id_obra_social, nombre, descripcion, porcentaje_descuento, es_particular);
+  };
+
+};
