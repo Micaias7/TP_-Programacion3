3a0d63583b004b6d974b93be7d0076d8d4c07702
Micaias <gutierrezmicaias@gmail.com>
2026-06-12 17:32:16 -0300

    Se separa el servidor del la configuracion de nuestra API, se instala morgan y se usa fs para fuardar logs de consultas

--- src/turnos.js
+++ src/turnos.js
@@ -1,19 +1,28 @@
 import express from "express";
+import morgan from "morgan";
+import fs from "fs";
+
 import { testConexion } from "./db/test_conexion.js";
 import { router as v1EspecialidadesRutas } from "./rutas/v1/especialidadesRutas.js";
 import { router as v1ObrasSocialesRutas } from "./rutas/v1/obrasSocialesRutas.js";
 import { router as v1MedicosRutas } from "./rutas/v1/medicosRutas.js";
-import { check, param } from "express-validator";
-import { validarCampos } from "./middlewares/validarCampos.js";
+// import { check, param } from "express-validator";
+// import { validarCampos } from "./middlewares/validarCampos.js";
 
 const app = express();
 
 await testConexion();
 
+let log = fs.createWriteStream('./accesos.log', { 
+    flags: 'a'
+});
+
+app.use(morgan('tiny'));
+app.use(morgan('combined', {stream: log}));
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  // console.log("test get");
   res.status(200).send({ estado: "ok", msg: "API OK" });
 });
 
@@ -22,9 +31,4 @@ app.use("/api/v1/obras-sociales", v1ObrasSocialesRutas);
 app.use("/api/v1/medicos", v1MedicosRutas);
 
 
-process.loadEnvFile();
-const PUERTO = process.env.PUERTO;
-
-app.listen(PUERTO || 3000, () => {
-  console.log(`servidor iniciado OK en puerto ${PUERTO}`);
-});
+export default app;
