1aa810c752fcd1a2e3a9f08101e0d43966c473d7
YamilaAyi <entreriosbazar@gmail.com>
2026-06-13 20:22:15 -0300

    cambio

--- src/turnos.js
+++ src/turnos.js
@@ -2,7 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import fs from "fs";
 import cors from "cors";
-// import passport from "passport";
+
 
 import { testConexion } from "./db/test_conexion.js";
 import { router as v1EspecialidadesRutas } from "./rutas/v1/especialidadesRutas.js";
@@ -25,15 +25,7 @@ app.use(morgan('combined', {stream: log}));
 app.use(cors());
 app.use(express.json());
 
-// passport.use(estrategia);
-// passport.use(validacion);
-// app.use(passport.initialize());
 
-// let log =fs.createWriteStream ('.accesos.log', {
-//   flags : 'a'
-// });
-// app.use(morgan('tiny'));
-// app.use(morgan ('combined',{stream:log}));
 
 app.get("/", (req, res) => {
   res.status(200).send({ estado: "ok", msg: "API OK" });
@@ -42,146 +34,6 @@ app.get("/", (req, res) => {
 app.use("/api/v1/especialidades", v1EspecialidadesRutas);
 app.use("/api/v1/obras-sociales", v1ObrasSocialesRutas);
 app.use("/api/v1/medicos", v1MedicosRutas);
-app.post(
-  "/especialidades",
-  [check("nombre", "El nombre es obligatorio").notEmpty(), validarCampos],
-  async (req, res) => {
-    try {
-      const { nombre } = req.body;
-
-      const sql = "INSERT INTO especialidades (nombre) VALUES (?)";
-
-      const [result] = await pool.execute(sql, [nombre]);
-
-      if (result.affectedRows > 0) {
-        res
-          .status(201)
-          .send({ estado: true, msg: `ID Creado ${result.insertId}` });
-      }
-    } catch (error) {
-      res.status(500).send({ estado: false, msg: "Error interno" });
-    }
-  },
-);
-
-app.put(
-  "/especialidades/:id_especialidad",
-  [
-    check("nombre")
-      .notEmpty()
-      .withMessage("El nombre es obligatorio")
-      .isLength({ max: 120 })
-      .withMessage("El nombre no debe ser mayor a 120 caracteres"),
-    param("id_especialidad", "El parametro debe ser numerico").isInt(),
-    validarCampos,
-  ],
-  async (req, res) => {
-    try {
-      const id_especialidad = req.params.id_especialidad;
-      const sqlb =
-        "SELECT * FROM especialidades WHERE activo = 1 AND id_especialidad = ?";
-
-      const [especialidades, fields] = await pool.execute(sqlb, [
-        id_especialidad,
-      ]);
-
-      if (especialidades.length === 0) {
-        return res
-          .status(404)
-          .send({ estado: false, msg: "Especialidad no encontrada" });
-      }
-
-      const { nombre } = req.body;
-
-      const sql =
-        "UPDATE especialidades SET nombre = ? WHERE id_especialidad = ?";
-
-      const [result] = await pool.execute(sql, [nombre, id_especialidad]);
-
-      if (result.affectedRows > 0) {
-        res.status(200).send({ estado: true, msg: `Especialidad modificada` });
-      }
-    } catch (error) {
-      res.status(500).send({ estado: false, msg: "Error interno" });
-    }
-  },
-);
-
-app.delete(
-  "/especialidades/:id_especialidad",
-  [
-    param("id_especialidad", "El parametro debe ser numerico").isInt(),
-    validarCampos,
-  ],
-  async (req, res) => {
-    try {
-      const id_especialidad = req.params.id_especialidad;
-      const sqlb =
-        "SELECT * FROM especialidades WHERE activo = 1 AND id_especialidad = ?";
-
-      const [especialidades, fields] = await pool.execute(sqlb, [
-        id_especialidad,
-      ]);
-
-      if (especialidades.length === 0) {
-        return res
-          .status(404)
-          .send({ estado: false, msg: "Especialidad no encontrada" });
-      }
-
-      const sql =
-        "UPDATE especialidades SET activo = 0 WHERE id_especialidad = ?";
-
-      const [result] = await pool.execute(sql, [id_especialidad]);
-
-      if (result.affectedRows > 0) {
-        res.status(200).send({ estado: true, msg: `Especialidad eliminada` });
-      }
-    } catch (error) {
-      res.status(500).send({ estado: false, msg: "Error interno" });
-    }
-  },
-);
-
-// app.get('/especialidades', async (req, res) => {
-
-//   try {
-//     const sql = 'SELECT * FROM especialidades WHERE activo = 1';
-
-//     const [especialidades, fields] = await pool.query(sql);
-
-//     res.status(200).send({
-//       'estado': 'ok',
-//       'especialidades': especialidades
-//     });
-
-//   } catch (error) {
-//     console.log(error);
-//   };
-// });
-
-app.get("/especialidades/:id_especialidad", async (req, res) => {
-  try {
-    const id_especialidad = req.params.id_especialidad;
-    const sql =
-      "SELECT * FROM especialidades WHERE activo = 1 AND id_especialidad = ?";
-    const [especialidades, fields] = await pool.execute(sql, [id_especialidad]);
-
-    res.status(200).send({
-      estado: "ok",
-      especialidades: especialidades,
-    });
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-process.loadEnvFile();
-const PUERTO = process.env.PUERTO;
-
-app.listen(PUERTO || 3000, () => {
-  console.log(`servidor iniciado OK en puerto ${PUERTO}`);
-});
 
 
 export default app;
