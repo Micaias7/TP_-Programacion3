b89b3a98e98b6cbc4c4c7f6f514c7c3ba5fb8ef4
Micaias <gutierrezmicaias@gmail.com>
2026-06-14 23:37:25 -0300

    pull con rama Yamila, ruta Put asociar OS con pacientes

--- src/db/conexion.js
+++ src/db/conexion.js
@@ -2,11 +2,6 @@ import mysql from 'mysql2/promise';
 
 process.loadEnvFile();
 
-console.log("DB_USER:", process.env.DB_USER);
-console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
-console.log("DB_HOST:", process.env.DB_HOST);
-console.log("DB_DATABASE:", process.env.DB_DATABASE);
-
 export const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
